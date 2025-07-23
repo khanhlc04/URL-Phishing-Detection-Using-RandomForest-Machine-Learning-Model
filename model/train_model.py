a1ffb1bc8a005a830d86e9ac6b7418316a0e3aef
khanhlc04 <150151244+khanhlc04@users.noreply.github.com>
2025-07-23 22:06:57 +0700

    add randoom forest and naive bayes

--- model/train_model.py
+++ model/train_model.py
@@ -3,6 +3,8 @@ import joblib
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier  
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 def train_model():
     df = pd.read_csv("Synthetic_Dataset/clean_phishing_dataset.csv")
@@ -11,13 +13,13 @@ def train_model():
     X = df.drop(columns=["url", "label"])
     y = df["label"]
     
-    feature_names = X.columns.tolist()  # ✅ Lưu lại tên cột
-    
     # Split dataset
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
     
     # Train model
-    model = DecisionTreeClassifier()
+    # model = DecisionTreeClassifier()
+    # model = RandomForestClassifier()
+    model = GaussianNB()
     model.fit(X_train, y_train)
 
     # Evaluate
@@ -26,8 +28,9 @@ def train_model():
     print("Accuracy Score:", acc)
 
     # Save model + feature names
-    joblib.dump(model, "phishing_model_decision_tree.pkl")
-    joblib.dump(feature_names, "phishing_model_features.pkl")  # ✅ Lưu tên feature
+    # joblib.dump(model, "phishing_model_decision_tree.pkl")
+    # joblib.dump(model, "phishing_model_randoom_forest.pkl")
+    joblib.dump(model, "phishing_model_naive_bayes.pkl")
 
     print("Model and feature names saved successfully.")
 
