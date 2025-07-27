37a82a819d5f1b85c3d50290c4747842f1271006
khanhlc04 <150151244+khanhlc04@users.noreply.github.com>
2025-07-27 23:12:29 +0700

    update

--- model/train_model.py
+++ model/train_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import joblib
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.tree import DecisionTreeClassifier  
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -31,6 +32,14 @@ def train_model():
     # Evaluate
     prediction = model.predict(X_test_scaled)
     acc = accuracy_score(y_test, prediction)
+    prec = precision_score(y_test, prediction)
+    rec = recall_score(y_test, prediction)
+    f1 = f1_score(y_test, prediction)
+    print("Accuracy Score:", acc)
+    print("Precision Score:", prec)
+    print("Recall Score:", rec)
+    print("F1 Score:", f1)
+
     print("Accuracy Score:", acc)
 
     # Save model và scaler
